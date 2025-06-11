2a23cac17ee48609ce04d462a05f25c8e43145de
Midgardsormrr <diego.guerra.d@usach.cl>
2025-06-11 05:26:06 -0400

    Api_url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // En Vite, las vars de entorno deben empezar con VITE_
 // y se acceden vía import.meta.env.VITE_<NOMBRE>
-const API_URL = import.meta.env.VITE_API_URL || "http://gateway-service:8080";
+const API_URL = import.meta.env.VITE_API_URL || "http://172.24.243.207:8080";
 
 const httpClient = axios.create({
   baseURL: API_URL,
